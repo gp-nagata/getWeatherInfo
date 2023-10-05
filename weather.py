1a655945c3bda85367d6f1cb410f9d5b0af3ae3d
gp-nagata <146795050+gp-nagata@users.noreply.github.com>
2023-10-05 13:54:35 +0900

    Add files via upload

--- weather.py
+++ weather.py
@@ -1,10 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-def GetYahooWeather(AreaCode):
-
+def get_yahoo_weather(area_code):
     # Yahoo天気情報のURLを生成
-    url = "https://weather.yahoo.co.jp/weather/jp/13/" + str(AreaCode) + ".html"
+    url = f"https://weather.yahoo.co.jp/weather/jp/13/{area_code}.html"
 
     # URLにアクセスしてHTMLを取得
     r = requests.get(url)
@@ -15,26 +14,14 @@ def GetYahooWeather(AreaCode):
     # 天気情報を含むHTML要素を取得
     weather_info = soup.find(class_='forecastCity')
 
-    # 改行文字を基準にテキストを分割してリストに格納
-    weather_info = [i.strip() for i in weather_info.text.splitlines()]
+    # テキストを整理
+    weather_info_text = [i.strip() for i in weather_info.text.splitlines() if i.strip()]
 
-    # 空の要素を削除
-    weather_info = [i for i in weather_info if i != ""]
+    # 今日の天気情報と気温を取得
+    today_date, today_weather, today_temp = weather_info_text[0], weather_info_text[1], weather_info_text[2]
 
-    # 今日の日付を取得
-    today = weather_info[0]
-
-    # 今日の天気を取得
-    today_weather = weather_info[1]
-
-    # 明日の日付を取得
-    tomorrow = weather_info[19]
-
-    # 明日の天気を取得
-    tomorrow_weather = weather_info[20]
-
-    # 天気情報を文字列として返す
-    return f"{today}の天気は{today_weather}、{tomorrow}の天気は{tomorrow_weather}です。"
+    # 天気情報と気温を文字列として返す
+    return f"{today_date}の天気は{today_weather}、気温は{today_temp}です。"
 
 # 関数を呼び出して天気情報を取得し、表示
-print(GetYahooWeather(4410))
+print(get_yahoo_weather(4410))
